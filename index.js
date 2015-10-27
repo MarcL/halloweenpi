f764750e4db6a3ca40445b183c4fa8d4e4d7f242
Marc Littlemore <marc.littlemore@gmail.com>
2015-10-27 22:01:32 +0000

    Add code to look for button press.

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
-var SoundPlayer = require('./lib/SoundPlayer');
+var PiPlayer = require('./lib/PiPlayer');
 
-console.log('Initialised...');
-
-var player = new SoundPlayer();
 var soundFiles = [
   './assets/test.mp3'
 ];
 
-var randomIndex = Math.floor(Math.random() * soundFiles.length);
-var randomSound = soundFiles[randomIndex];
+var buttonGpio = 4;
+var ledGpio = 14;
 
-console.log('Playing : ' + randomSound);
-player.play(randomSound, function(error) {
-  console.log('Finished : ' + randomSound);
-  console.log(error);
-});
+var player = new PiPlayer(soundFiles, buttonGpio, ledGpio);
 
-console.log('Shutdown...');
+
