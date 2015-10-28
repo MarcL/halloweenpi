702fae34dd15d7c95b92f2ecba4dfd222d322b3c
Marc Littlemore <marc.littlemore@gmail.com>
2015-10-28 23:10:02 +0000

    Add new sound effects.

--- config/config.js
+++ config/config.js
@@ -6,7 +6,15 @@ module.exports = {
         './assets/evil-laugh.mp3',
         './assets/female-scream.mp3',
         './assets/funeral-bells.mp3',
-        './assets/howling-wolf.mp3'
+        './assets/howling-wolf.mp3',
+        './assets/cat-scream.mp3',
+        './assets/woman-shrill-scream.mp3',
+        './assets/thriller-laugh.mp3',
+        './assets/glados-hello-friend.mp3',
+        './assets/glados-hello-where-are-you.mp3',
+        './assets/glados-i-see-you.mp3',
+        './assets/glados-laugh.mp3',
+        './assets/glados-come-over-here.mp3'
 	],
 	buttonGpio: 4,
 	ledGpio: 14
