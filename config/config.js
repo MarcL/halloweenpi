818ad9f53d08a7da6aed3f83b5d529617de2ab0f
Marc Littlemore <marc.littlemore@gmail.com>
2015-10-28 22:24:46 +0000

    Add test sound effects.

--- config/config.js
+++ config/config.js
@@ -1,6 +1,12 @@
 module.exports = {
 	soundFiles: [
-		'./assets/test.mp3'
+		'./assets/cuckoo-clock.mp3',
+        './assets/foghorn-doorbell.mp3',
+        './assets/ghost-scream.mp3',
+        './assets/evil-laugh.mp3',
+        './assets/female-scream.mp3',
+        './assets/funeral-bells.mp3',
+        './assets/howling-wolf.mp3'
 	],
 	buttonGpio: 4,
 	ledGpio: 14
